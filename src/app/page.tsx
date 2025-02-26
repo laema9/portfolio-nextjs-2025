d0ffc760941c01e02c5926b35dc80a863bbce8f3
laema9 <laema9@gmail.com>
2025-02-26 16:37:35 +0100

    Add ShadCNUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { HeroGeometric } from "@/components/ui/shape-landing-hero"
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p>LAEMA9 PORTFOLIO</p>
-      <p>🚧🚧🚧</p>
+    <div>
+      <HeroGeometric badge="Title 1"
+          title1 = "Title 2"
+          title2 = "Title 3" 
+          description = "Description Test" 
+          />
     </div>
   );
 }
